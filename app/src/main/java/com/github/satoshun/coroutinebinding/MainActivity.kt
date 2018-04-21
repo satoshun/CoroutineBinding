a43976669540edcfe2659625780b51d4981dfda4
SatoShun <shun.sato1@gmail.com>
2018-04-21 09:10:25 +0000

    first

---
+++ app/src/main/java/com/github/satoshun/coroutinebinding/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.github.satoshun.coroutinebinding
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
