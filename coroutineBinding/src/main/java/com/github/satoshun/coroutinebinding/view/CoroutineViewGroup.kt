6f001b89f13dab93f3dddb3942fa3f8713296380
satoshun <shun.sato1@gmail.com>
2018-04-23 23:08:50 +0000

    add ViewGroup.changeEvents

---
+++ coroutineBinding/src/main/java/com/github/satoshun/coroutinebinding/view/CoroutineViewGroup.kt
@@ -0,0 +1,43 @@
+@file:Suppress("NOTHING_TO_INLINE")
+
+package com.github.satoshun.coroutinebinding.view
+
+import android.view.View
+import android.view.ViewGroup
+import com.github.satoshun.coroutinebinding.cancelableChannel
+import com.github.satoshun.coroutinebinding.safeOffer
+import kotlinx.coroutines.experimental.channels.ReceiveChannel
+
+/**
+ * todo
+ */
+inline fun ViewGroup.changeEvents(): ReceiveChannel<ViewGroupHierarchyChangeEvent> = cancelableChannel {
+  val listener = object : ViewGroup.OnHierarchyChangeListener {
+    override fun onChildViewRemoved(parent: View, child: View) {
+      safeOffer(ViewGroupHierarchyChildViewAddEvent(this@changeEvents, child))
+    }
+
+    override fun onChildViewAdded(parent: View, child: View) {
+      safeOffer(ViewGroupHierarchyChildViewRemoveEvent(this@changeEvents, child))
+    }
+  }
+  onAfterClosed = {
+    setOnHierarchyChangeListener(null)
+  }
+  setOnHierarchyChangeListener(listener)
+}
+
+sealed class ViewGroupHierarchyChangeEvent(
+    val view: ViewGroup,
+    val child: View
+)
+
+class ViewGroupHierarchyChildViewAddEvent(
+    view: ViewGroup,
+    child: View
+) : ViewGroupHierarchyChangeEvent(view, child)
+
+class ViewGroupHierarchyChildViewRemoveEvent(
+    view: ViewGroup,
+    child: View
+) : ViewGroupHierarchyChangeEvent(view, child)
