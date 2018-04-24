7d4763ae60dcce69edd591683b6b5254ec7b17df
satoshun <shun.sato1@gmail.com>
2018-04-24 09:57:09 +0000

    add Adapter.dataChanges

---
+++ coroutineBinding/src/main/java/com/github/satoshun/coroutinebinding/widget/CoroutineAdapter.kt
@@ -0,0 +1,19 @@
+package com.github.satoshun.coroutinebinding.widget
+
+import android.database.DataSetObserver
+import android.widget.Adapter
+import com.github.satoshun.coroutinebinding.cancelableChannel
+import com.github.satoshun.coroutinebinding.safeOffer
+import kotlinx.coroutines.experimental.channels.ReceiveChannel
+
+fun <T : Adapter> T.dataChanges(): ReceiveChannel<T> = cancelableChannel {
+  val listener = object : DataSetObserver() {
+    override fun onChanged() {
+      safeOffer(this@dataChanges)
+    }
+  }
+  onAfterClosed = {
+    registerDataSetObserver(null)
+  }
+  registerDataSetObserver(listener)
+}
