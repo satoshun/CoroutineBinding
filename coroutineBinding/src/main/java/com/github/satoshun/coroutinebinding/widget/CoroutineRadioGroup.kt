da838cd7c9d65adf0932e1612dad68b7f81628e3
satoshun <shun.sato1@gmail.com>
2018-04-25 00:02:41 +0000

    add RadioGroup.checkedChanges

---
+++ coroutineBinding/src/main/java/com/github/satoshun/coroutinebinding/widget/CoroutineRadioGroup.kt
@@ -0,0 +1,18 @@
+@file:Suppress("NOTHING_TO_INLINE")
+
+package com.github.satoshun.coroutinebinding.widget
+
+import android.widget.RadioGroup
+import com.github.satoshun.coroutinebinding.cancelableChannel
+import com.github.satoshun.coroutinebinding.safeOffer
+import kotlinx.coroutines.experimental.channels.ReceiveChannel
+
+inline fun RadioGroup.checkedChanges(): ReceiveChannel<Int> = cancelableChannel {
+  val listener = RadioGroup.OnCheckedChangeListener { _, checkedId ->
+    safeOffer(checkedId)
+  }
+  onAfterClosed = {
+    setOnCheckedChangeListener(null)
+  }
+  setOnCheckedChangeListener(listener)
+}
