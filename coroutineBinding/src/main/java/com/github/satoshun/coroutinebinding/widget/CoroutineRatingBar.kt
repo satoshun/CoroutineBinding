e25e1844711dab7656bdeae2b386c4555dee24c9
satoshun <shun.sato1@gmail.com>
2018-04-25 00:06:42 +0000

    add RatingBar

---
+++ coroutineBinding/src/main/java/com/github/satoshun/coroutinebinding/widget/CoroutineRatingBar.kt
@@ -0,0 +1,34 @@
+@file:Suppress("NOTHING_TO_INLINE")
+
+package com.github.satoshun.coroutinebinding.widget
+
+import android.widget.RatingBar
+import com.github.satoshun.coroutinebinding.cancelableChannel
+import com.github.satoshun.coroutinebinding.safeOffer
+import kotlinx.coroutines.experimental.channels.ReceiveChannel
+
+inline fun RatingBar.ratingChanges(): ReceiveChannel<Float> = cancelableChannel {
+  val listener = RatingBar.OnRatingBarChangeListener { _, rating, _ ->
+    safeOffer(rating)
+  }
+  onAfterClosed = {
+    onRatingBarChangeListener = null
+  }
+  onRatingBarChangeListener = listener
+}
+
+inline fun RatingBar.ratingChangeEvents(): ReceiveChannel<RatingBarChangeEvent> = cancelableChannel {
+  val listener = RatingBar.OnRatingBarChangeListener { ratingBar, rating, fromUser ->
+    safeOffer(RatingBarChangeEvent(ratingBar, rating, fromUser))
+  }
+  onAfterClosed = {
+    onRatingBarChangeListener = null
+  }
+  onRatingBarChangeListener = listener
+}
+
+data class RatingBarChangeEvent(
+    val view: RatingBar,
+    val rating: Float,
+    val fromUser: Boolean
+)
