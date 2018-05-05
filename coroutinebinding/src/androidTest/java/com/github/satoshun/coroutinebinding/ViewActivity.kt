cce712af82b31fd491db786c00f39feb9b20f508
SatoShun <shun.sato1@gmail.com>
2018-05-05 05:27:38 +0000

    add recyclerview test

--- coroutinebinding/src/androidTest/java/com/github/satoshun/coroutinebinding/ViewActivity.kt
+++ coroutinebinding/src/androidTest/java/com/github/satoshun/coroutinebinding/ViewActivity.kt
@@ -6,7 +6,7 @@ import android.view.ViewGroup
 import android.widget.FrameLayout
 
 class ViewActivity : Activity() {
-  val rootView: ViewGroup get() = findViewById(android.R.id.content)
+  val rootView: ViewGroup get() = view
   lateinit var view: ViewGroup
 
   override fun onCreate(savedInstanceState: Bundle?) {
