300be8e475506e18b169aa4dae1c0597bceb3e1b
satoshun <shun.sato1@gmail.com>
2018-05-08 23:26:32 +0000

    PopupMenu.itemClicks

--- coroutinebinding-appcompat-v7/src/main/java/com/github/satoshun/coroutinebinding/support/v7/widget/CoroutinePopupMenu.kt
+++ coroutinebinding-appcompat-v7/src/main/java/com/github/satoshun/coroutinebinding/support/v7/widget/CoroutinePopupMenu.kt
@@ -3,18 +3,18 @@
 package com.github.satoshun.coroutinebinding.support.v7.widget
 
 import android.support.annotation.CheckResult
-import android.support.v7.widget.ActionMenuView
+import android.support.v7.widget.PopupMenu
 import android.view.MenuItem
 import com.github.satoshun.coroutinebinding.cancelableChannel
 import com.github.satoshun.coroutinebinding.safeOffer
 import kotlinx.coroutines.experimental.channels.ReceiveChannel
 
 /**
- * Create an channel which emits the clicked menu item in view.
+ * Create an channel which emits the clicked item in [View]s menu.
  */
 @CheckResult
-inline fun ActionMenuView.itemClicks(capacity: Int = 0): ReceiveChannel<MenuItem> = cancelableChannel(capacity) { onAfterClosed ->
-  val listener = ActionMenuView.OnMenuItemClickListener {
+inline fun PopupMenu.itemClicks(capacity: Int = 0): ReceiveChannel<MenuItem> = cancelableChannel(capacity) { onAfterClosed ->
+  val listener = PopupMenu.OnMenuItemClickListener {
     safeOffer(it)
     true
   }
