c804dbd5947d7709690d72e6b1c9c1ae0cd16e25
satoshun <shun.sato1@gmail.com>
2018-05-09 05:58:48 +0000

    AppBarLayout.offsetChanges

--- coroutinebinding-design/src/main/java/com/github/satoshun/coroutinebinding/design/widget/CoroutineAppBarLayout.kt
+++ coroutinebinding-design/src/main/java/com/github/satoshun/coroutinebinding/design/widget/CoroutineAppBarLayout.kt
@@ -1,1 +1,21 @@
+@file:Suppress("NOTHING_TO_INLINE")
+
 package com.github.satoshun.coroutinebinding.design.widget
+
+import android.support.design.widget.AppBarLayout
+import com.github.satoshun.coroutinebinding.cancelableChannel
+import com.github.satoshun.coroutinebinding.safeOffer
+import kotlinx.coroutines.experimental.channels.ReceiveChannel
+
+/**
+ * Create an channel which emits the offset change in view.
+ */
+inline fun AppBarLayout.offsetChanges(capacity: Int = 0): ReceiveChannel<Int> = cancelableChannel(capacity) { onAfterClosed ->
+  val listener = AppBarLayout.OnOffsetChangedListener { _, offset ->
+    safeOffer(offset)
+  }
+  onAfterClosed {
+    removeOnOffsetChangedListener(listener)
+  }
+  addOnOffsetChangedListener(listener)
+}
