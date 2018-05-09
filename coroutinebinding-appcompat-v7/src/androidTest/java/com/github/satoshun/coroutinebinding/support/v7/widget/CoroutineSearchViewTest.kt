93bc370670043c51c6eef026569ceed5f0781a66
satoshun <shun.sato1@gmail.com>
2018-05-09 00:12:15 +0000

    SearchView.queryTextChangeEvents

---
+++ coroutinebinding-appcompat-v7/src/androidTest/java/com/github/satoshun/coroutinebinding/support/v7/widget/CoroutineSearchViewTest.kt
@@ -0,0 +1,52 @@
+package com.github.satoshun.coroutinebinding.support.v7.widget
+
+import android.support.test.annotation.UiThreadTest
+import android.support.test.rule.ActivityTestRule
+import android.support.v7.view.ContextThemeWrapper
+import android.support.v7.widget.SearchView
+import com.github.satoshun.coroutinebinding.isEqualTo
+import com.github.satoshun.coroutinebinding.isNull
+import com.github.satoshun.coroutinebinding.support.v7.ViewActivity
+import com.github.satoshun.coroutinebinding.support.v7.appcompat.R
+import com.github.satoshun.coroutinebinding.testRunBlocking
+import com.github.satoshun.coroutinebinding.uiLaunch
+import com.github.satoshun.coroutinebinding.uiRunBlocking
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class CoroutineSearchViewTest {
+  @JvmField @Rule val rule = ActivityTestRule<ViewActivity>(ViewActivity::class.java)
+
+  private lateinit var searchView: SearchView
+
+  @Before @UiThreadTest
+  fun setUp() {
+    val context = ContextThemeWrapper(rule.activity, R.style.Theme_AppCompat)
+    searchView = SearchView(context)
+    rule.activity.view.addView(searchView)
+  }
+
+  @Test
+  fun queryTextChangeEvents() = testRunBlocking {
+    uiRunBlocking { searchView.setQuery("init", false) }
+    val queryTextChangeEvents = uiRunBlocking { searchView.queryTextChangeEvents(2) }
+
+    uiLaunch { searchView.setQuery("Hi", false) }
+    queryTextChangeEvents.receive().queryText.isEqualTo("Hi")
+    uiLaunch { searchView.setQuery("Hoi", false) }
+    queryTextChangeEvents.receive().queryText.isEqualTo("Hoi")
+    uiLaunch { searchView.setQuery(null, false) }
+    queryTextChangeEvents.receive().queryText.isEqualTo("")
+
+    uiLaunch { searchView.setQuery("Hello", true) }
+    // change event
+    queryTextChangeEvents.receive().isSubmitted.isEqualTo(false)
+    // submitted event
+    queryTextChangeEvents.receive().isSubmitted.isEqualTo(true)
+
+    queryTextChangeEvents.cancel()
+    uiLaunch { searchView.setQuery("Hoi", false) }
+    queryTextChangeEvents.receiveOrNull().isNull()
+  }
+}
---
+++ coroutinebinding-appcompat-v7/src/main/java/com/github/satoshun/coroutinebinding/support/v7/widget/CoroutineSearchView.kt
@@ -0,0 +1,35 @@
+@file:Suppress("NOTHING_TO_INLINE")
+
+package com.github.satoshun.coroutinebinding.support.v7.widget
+
+import android.support.annotation.CheckResult
+import android.support.v7.widget.SearchView
+import com.github.satoshun.coroutinebinding.cancelableChannel
+import com.github.satoshun.coroutinebinding.safeOffer
+import kotlinx.coroutines.experimental.channels.ReceiveChannel
+
+/**
+ * Create an channel which emits the clicked item in views menu.
+ */
+@CheckResult
+inline fun SearchView.queryTextChangeEvents(capacity: Int = 0): ReceiveChannel<SearchViewQueryTextEvent> = cancelableChannel(capacity) { onAfterClosed ->
+  val listener = object : SearchView.OnQueryTextListener {
+    override fun onQueryTextSubmit(query: String?): Boolean {
+      return safeOffer(SearchViewQueryTextEvent(this@queryTextChangeEvents, this@queryTextChangeEvents.query, true))
+    }
+
+    override fun onQueryTextChange(newText: String): Boolean {
+      return safeOffer(SearchViewQueryTextEvent(this@queryTextChangeEvents, newText, false))
+    }
+  }
+  onAfterClosed {
+    setOnQueryTextListener(null)
+  }
+  setOnQueryTextListener(listener)
+}
+
+data class SearchViewQueryTextEvent(
+    val view: SearchView,
+    val queryText: CharSequence,
+    val isSubmitted: Boolean
+)
