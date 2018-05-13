9b5eed501e21d8351fdca25fd5196dba266c1428
satoshun <shun.sato1@gmail.com>
2018-05-13 00:59:17 +0000

    coroutinebinding-recycler-view: add javadoc and remove inline modifier some methods

--- coroutinebinding-recyclerview-v7/src/main/java/com/github/satoshun/coroutinebinding/support/v7/widget/CoroutineRecyclerViewAdapter.kt
+++ coroutinebinding-recyclerview-v7/src/main/java/com/github/satoshun/coroutinebinding/support/v7/widget/CoroutineRecyclerViewAdapter.kt
@@ -1,5 +1,3 @@
-@file:Suppress("NOTHING_TO_INLINE")
-
 package com.github.satoshun.coroutinebinding.support.v7.widget
 
 import android.support.v7.widget.RecyclerView
@@ -8,9 +6,9 @@ import com.github.satoshun.coroutinebinding.safeOffer
 import kotlinx.coroutines.experimental.channels.ReceiveChannel
 
 /**
- * Create an channel of data change events for [RecyclerView.Adapter].
+ * Create an channel of data change events for RecyclerView.Adapter.
  */
-inline fun <T : RecyclerView.Adapter<out RecyclerView.ViewHolder>> T.dataChanges(capacity: Int = 0): ReceiveChannel<T> = cancelableChannel(capacity) { onAfterClosed ->
+fun <T : RecyclerView.Adapter<out RecyclerView.ViewHolder>> T.dataChanges(capacity: Int = 0): ReceiveChannel<T> = cancelableChannel(capacity) { onAfterClosed ->
   val listener = object : RecyclerView.AdapterDataObserver() {
     override fun onChanged() {
       safeOffer(this@dataChanges)
