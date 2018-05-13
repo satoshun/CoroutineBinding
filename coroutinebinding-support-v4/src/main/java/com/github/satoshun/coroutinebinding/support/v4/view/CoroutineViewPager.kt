9897fa2f52034fcbeec0a18c7cdfdd0f7d9e805d
satoshun <shun.sato1@gmail.com>
2018-05-13 01:00:33 +0000

    coroutinebinding-recycler-view: add javadoc and add CheckResult annotation

--- coroutinebinding-support-v4/src/main/java/com/github/satoshun/coroutinebinding/support/v4/view/CoroutineViewPager.kt
+++ coroutinebinding-support-v4/src/main/java/com/github/satoshun/coroutinebinding/support/v4/view/CoroutineViewPager.kt
@@ -1,5 +1,6 @@
 package com.github.satoshun.coroutinebinding.support.v4.view
 
+import android.support.annotation.CheckResult
 import android.support.v4.view.ViewPager
 import com.github.satoshun.coroutinebinding.cancelableChannel
 import com.github.satoshun.coroutinebinding.safeOffer
@@ -8,6 +9,7 @@ import kotlinx.coroutines.experimental.channels.ReceiveChannel
 /**
  * Create an channel of scroll state change events on view.
  */
+@CheckResult
 fun ViewPager.pageScrollStateChanges(capacity: Int = 0): ReceiveChannel<Int> = cancelableChannel(capacity) { onAfterClosed ->
   val listener = object : ViewPager.OnPageChangeListener {
     override fun onPageScrollStateChanged(state: Int) {
@@ -29,6 +31,7 @@ fun ViewPager.pageScrollStateChanges(capacity: Int = 0): ReceiveChannel<Int> = c
 /**
  * CreateCreate an channel of page selected events on view.
  */
+@CheckResult
 fun ViewPager.pageSelections(capacity: Int = 0): ReceiveChannel<Int> = cancelableChannel(capacity) { onAfterClosed ->
   val listener = object : ViewPager.OnPageChangeListener {
     override fun onPageScrollStateChanged(state: Int) {
