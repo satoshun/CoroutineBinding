21661d9291bd2eca4b83ec5bfa5d3c2eada85749
satoshun <shun.sato1@gmail.com>
2018-05-13 00:52:55 +0000

    coroutinebinding-appcompat-v7: add javadoc and remove inline modifier some methods

--- coroutinebinding-appcompat-v7/src/main/java/com/github/satoshun/coroutinebinding/support/v7/widget/CoroutineActionMenuView.kt
+++ coroutinebinding-appcompat-v7/src/main/java/com/github/satoshun/coroutinebinding/support/v7/widget/CoroutineActionMenuView.kt
@@ -1,5 +1,3 @@
-@file:Suppress("NOTHING_TO_INLINE")
-
 package com.github.satoshun.coroutinebinding.support.v7.widget
 
 import android.support.annotation.CheckResult
@@ -13,7 +11,7 @@ import kotlinx.coroutines.experimental.channels.ReceiveChannel
  * Create an channel which emits the clicked menu item in view.
  */
 @CheckResult
-inline fun ActionMenuView.itemClicks(capacity: Int = 0): ReceiveChannel<MenuItem> = cancelableChannel(capacity) { onAfterClosed ->
+fun ActionMenuView.itemClicks(capacity: Int = 0): ReceiveChannel<MenuItem> = cancelableChannel(capacity) { onAfterClosed ->
   val listener = ActionMenuView.OnMenuItemClickListener {
     safeOffer(it)
     true
