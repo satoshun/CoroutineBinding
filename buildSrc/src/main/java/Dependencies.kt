d2df833c9230a95aadb7423ccd9ff777ed223802
satoshun <shun.sato1@gmail.com>
2018-07-04 23:29:23 +0000

    agp 3.2.0-beta2

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -2,7 +2,7 @@ object Vers {
   val compile_sdk = 27
   val min_sdk = 15
   val target_sdk = 27
-  val agp = "3.1.3"
+  val agp = "3.2.0-beta02"
 
   val kotlin = "1.2.50"
   val couroutine = "0.23.4"
