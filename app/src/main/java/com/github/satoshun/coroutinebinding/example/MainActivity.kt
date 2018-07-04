72d46c84a68bb53d3735e4e6a430d9fbb7f545a8
satoshun <shun.sato1@gmail.com>
2018-07-04 23:29:09 +0000

    detach and attach example

--- app/src/main/java/com/github/satoshun/coroutinebinding/example/MainActivity.kt
+++ app/src/main/java/com/github/satoshun/coroutinebinding/example/MainActivity.kt
@@ -3,8 +3,8 @@ package com.github.satoshun.coroutinebinding.example
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import android.widget.TextView
 import com.github.satoshun.coroutinebinding.view.attaches
+import com.github.satoshun.coroutinebinding.view.detaches
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.experimental.android.UI
 import kotlinx.coroutines.experimental.delay
@@ -16,26 +16,25 @@ class MainActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
-    val text = TextView(this)
-
     launch(UI) {
-      for (attach in text.attaches()) {
+      for (attach in hello.attaches()) {
         Log.d("attach", attach.toString())
       }
     }
 
     launch(UI) {
-      val attach = text.attaches().receive()
-      Log.d("attach from receive", attach.toString())
+      for (detach in hello.detaches()) {
+        Log.d("detach", detach.toString())
+      }
     }
 
     launch(UI) {
       while (true) {
-        delay(2000, TimeUnit.MILLISECONDS)
-        if (!text.isAttachedToWindow) {
-          root.addView(text)
-        } else {
-          root.removeView(text)
+        delay(5000, TimeUnit.MILLISECONDS)
+        try {
+          root.addView(hello)
+        } catch (e: Exception) {
+          root.removeView(hello)
         }
       }
     }
