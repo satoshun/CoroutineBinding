4232f1d620275c5b13a7e1b1fd88227cc7fbeace
satoshun <shun.sato1@gmail.com>
2018-07-04 23:40:23 +0000

    kotlin 1.2.51

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -4,7 +4,7 @@ object Vers {
   val target_sdk = 27
   val agp = "3.2.0-beta02"
 
-  val kotlin = "1.2.50"
+  val kotlin = "1.2.51"
   val couroutine = "0.23.4"
   val support_lib = "27.1.1"
   val ktlint = "0.23.1"
