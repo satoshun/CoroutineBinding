09c9c6e62b36fc1f5cf165465387fb2bd77f43ee
SatoShun <shun.sato1@gmail.com>
2018-07-21 09:20:28 +0000

    design ktlint

--- coroutinebinding-design/src/main/java/com/github/satoshun/coroutinebinding/design/widget/CoroutineTabLayout.kt
+++ coroutinebinding-design/src/main/java/com/github/satoshun/coroutinebinding/design/widget/CoroutineTabLayout.kt
@@ -10,59 +10,61 @@ import kotlinx.coroutines.experimental.channels.ReceiveChannel
  * Create an channel which emits the selected tab in view.
  */
 @CheckResult
-fun TabLayout.selections(capacity: Int = 0): ReceiveChannel<TabLayout.Tab> = cancelableChannel(capacity) { onAfterClosed ->
-  val listener = object : TabLayout.OnTabSelectedListener {
-    override fun onTabReselected(tab: TabLayout.Tab?) {
-    }
+fun TabLayout.selections(capacity: Int = 0): ReceiveChannel<TabLayout.Tab> =
+  cancelableChannel(capacity) { onAfterClosed ->
+    val listener = object : TabLayout.OnTabSelectedListener {
+      override fun onTabReselected(tab: TabLayout.Tab?) {
+      }
 
-    override fun onTabUnselected(tab: TabLayout.Tab?) {
-    }
+      override fun onTabUnselected(tab: TabLayout.Tab?) {
+      }
 
-    override fun onTabSelected(tab: TabLayout.Tab) {
-      safeOffer(tab)
+      override fun onTabSelected(tab: TabLayout.Tab) {
+        safeOffer(tab)
+      }
     }
-  }
-  onAfterClosed {
-    removeOnTabSelectedListener(listener)
-  }
-  addOnTabSelectedListener(listener)
+    onAfterClosed {
+      removeOnTabSelectedListener(listener)
+    }
+    addOnTabSelectedListener(listener)
 
-  // todo this initialize code make sense on Channel?
+    // todo this initialize code make sense on Channel?
 //  val index = selectedTabPosition
 //  if (index != -1) {
 //    safeOffer(getTabAt(index)!!)
 //  }
-}
+  }
 
 /**
  * Create an channel which emits selection, reselection, and unselection events for the tabs in view.
  */
 @CheckResult
-fun TabLayout.selectionEvents(capacity: Int = 0): ReceiveChannel<TabLayoutSelectionEvent> = cancelableChannel(capacity) { onAfterClosed ->
-  val listener = object : TabLayout.OnTabSelectedListener {
-    override fun onTabReselected(tab: TabLayout.Tab) {
-      safeOffer(TabLayoutSelectionReselectedEvent(this@selectionEvents, tab))
-    }
+fun TabLayout.selectionEvents(capacity: Int = 0): ReceiveChannel<TabLayoutSelectionEvent> =
+  cancelableChannel(capacity) { onAfterClosed ->
+    val listener = object : TabLayout.OnTabSelectedListener {
+      override fun onTabReselected(tab: TabLayout.Tab) {
+        safeOffer(TabLayoutSelectionReselectedEvent(this@selectionEvents, tab))
+      }
 
-    override fun onTabUnselected(tab: TabLayout.Tab) {
-      safeOffer(TabLayoutSelectionUnselectedEvent(this@selectionEvents, tab))
-    }
+      override fun onTabUnselected(tab: TabLayout.Tab) {
+        safeOffer(TabLayoutSelectionUnselectedEvent(this@selectionEvents, tab))
+      }
 
-    override fun onTabSelected(tab: TabLayout.Tab) {
-      safeOffer(TabLayoutSelectionSelectedEvent(this@selectionEvents, tab))
+      override fun onTabSelected(tab: TabLayout.Tab) {
+        safeOffer(TabLayoutSelectionSelectedEvent(this@selectionEvents, tab))
+      }
     }
-  }
-  onAfterClosed {
-    removeOnTabSelectedListener(listener)
-  }
-  addOnTabSelectedListener(listener)
+    onAfterClosed {
+      removeOnTabSelectedListener(listener)
+    }
+    addOnTabSelectedListener(listener)
 
-  // todo this initialize code make sense on Channel?
+    // todo this initialize code make sense on Channel?
 //  val index = selectedTabPosition
 //  if (index != -1) {
 //    safeOffer(TabLayoutSelectionSelectedEvent(this@selectionEvents, getTabAt(index)!!))
 //  }
-}
+  }
 
 /**
  * A tab select event on TabLayout
@@ -76,22 +78,22 @@ sealed class TabLayoutSelectionEvent {
  * A tab selected event on TabLayout
  */
 data class TabLayoutSelectionSelectedEvent(
-    override val view: TabLayout,
-    override val tab: TabLayout.Tab
+  override val view: TabLayout,
+  override val tab: TabLayout.Tab
 ) : TabLayoutSelectionEvent()
 
 /**
  * A tab unselected event on TabLayout
  */
 data class TabLayoutSelectionUnselectedEvent(
-    override val view: TabLayout,
-    override val tab: TabLayout.Tab
+  override val view: TabLayout,
+  override val tab: TabLayout.Tab
 ) : TabLayoutSelectionEvent()
 
 /**
  * A tab reselected event on TabLayout
  */
 data class TabLayoutSelectionReselectedEvent(
-    override val view: TabLayout,
-    override val tab: TabLayout.Tab
+  override val view: TabLayout,
+  override val tab: TabLayout.Tab
 ) : TabLayoutSelectionEvent()
