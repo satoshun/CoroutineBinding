6a21227d9eabbc2de36d34f3aaf4ff2c533f78de
SatoShun <shun.sato1@gmail.com>
2018-07-27 23:56:38 +0000

    use new cancelable coroutine in coroutinebinding-constraintlayout module

--- coroutinebinding-constraintlayout/src/main/java/com/github/satoshun/coroutinebinding/constraintlayout/motion/CoroutineMotionLayout.kt
+++ coroutinebinding-constraintlayout/src/main/java/com/github/satoshun/coroutinebinding/constraintlayout/motion/CoroutineMotionLayout.kt
@@ -2,7 +2,8 @@ package com.github.satoshun.coroutinebinding.constraintlayout.motion
 
 import android.support.annotation.CheckResult
 import androidx.constraintlayout.motion.widget.MotionLayout
-import com.github.satoshun.coroutinebinding.cancelableChannel
+import com.github.satoshun.coroutinebinding.cancelableChannel2
+import com.github.satoshun.coroutinebinding.invokeOnCloseOnMain
 import com.github.satoshun.coroutinebinding.safeOffer
 import kotlinx.coroutines.experimental.channels.ReceiveChannel
 
@@ -11,35 +12,36 @@ import kotlinx.coroutines.experimental.channels.ReceiveChannel
  */
 @CheckResult
 fun MotionLayout.transitionChanged(capacity: Int = 0): ReceiveChannel<MotionLayoutTransition> =
-  cancelableChannel(capacity) {
-    val listener = object : MotionLayout.TransitionListener {
-      override fun onTransitionChange(motionLayout: MotionLayout, startId: Int, endId: Int, progress: Float) {
-        safeOffer(
-            MotionLayoutTransitionChanged(
-                motionLayout,
-                startId,
-                endId,
-                progress
-            )
-        )
-      }
+    cancelableChannel2(capacity) {
+      val listener = object : MotionLayout.TransitionListener {
+        override fun onTransitionChange(motionLayout: MotionLayout, startId: Int, endId: Int, progress: Float) {
+          safeOffer(
+              MotionLayoutTransitionChanged(
+                  motionLayout,
+                  startId,
+                  endId,
+                  progress
+              )
+          )
+        }
 
-      override fun onTransitionCompleted(motionLayout: MotionLayout, currentId: Int) {
-        safeOffer(
-            MotionLayoutTransitionCompleted(
-                motionLayout,
-                currentId
-            )
-        )
+        override fun onTransitionCompleted(motionLayout: MotionLayout, currentId: Int) {
+          safeOffer(
+              MotionLayoutTransitionCompleted(
+                  motionLayout,
+                  currentId
+              )
+          )
+        }
       }
+      invokeOnCloseOnMain {
+        setTransitionListener(null)
+      }
+      setTransitionListener(listener)
     }
-    it {
-      setTransitionListener(null)
-    }
-    setTransitionListener(listener)
-  }
 
 sealed class MotionLayoutTransition
+
 data class MotionLayoutTransitionChanged(
   val view: MotionLayout,
   val startId: Int,
